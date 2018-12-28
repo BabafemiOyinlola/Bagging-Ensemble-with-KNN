f98cfccca0971667e13f9f6d0cfa50d7f50af75e
Oyinlola <babsoyinlola@yahoo.com>
2018-12-28 21:51:17 +0000

    Initial commit

--- KNN.py
+++ KNN.py
@@ -12,13 +12,18 @@ class KNN:
 
         return math.sqrt(total)
 
+    def fit(self, train_feature, train_label):
+        self.train_feature = train_feature
+        self.train_label = train_label
+
     def k_neighbors(self, train_set, test_set):
         distances, neighbors = [], []
-
         #iterate till len(train_set) - 1 assuming the features are in the last column
-        for i in range(len(train_set) - 1):
+        for i in range(len(train_set)):
             distance = self.euclidean_distance(train_set[i], test_set)
+            train_set[i].append(self.train_label[i])
             distances.append((train_set[i], distance))
+
 
         #sort the distances
         distances = sorted(distances, key=lambda tup: tup[1])
@@ -45,16 +50,38 @@ class KNN:
         
         return label
 
-    def predict(self, train_set, test_set):
-
-        for each in test_set:
-            label = self.k_neighbors(train_set, each)
+    def predict(self, test_feature):
+        # train_set = np.column_stack([self.train_feature, self.train_label]).astype(np.float)
+        test_feature = test_feature
+        predicted_label = []
+        for each in test_feature:
+            label = self.k_neighbors(self.train_feature, each)
             each.append(label)
+            predicted_label.append(label)
         
-        return test_set
+        return predicted_label
 
+    def confussion_matrix(self,true_label, pred_label):
+        true_classes, pred_classes = {}, {}
+        for label in true_label:
+            if label in true_classes:
+                true_classes[label] += 1
+            else:
+                true_classes[label] == 1
+
+        for label in pred_label:
+            if label in pred_classes:
+                pred_classes[label] += 1
+            else:
+                pred_classes[label] == 1
+        
 
 KNN = KNN(2)
+X_train = [[7, 8, 9], [4, 4, 4],[2, 2, 2]]
+y_train = ['a', 'b', 'c']
+X_test = [[5, 5, 5], [9, 7, 5]]
+
+KNN.fit(X_train, y_train)
+y_pred = KNN.predict(X_test)
+
 trainSet = [[2, 2, 2, 'a'], [4, 4, 4, 'b'], [7, 8, 9, 'c']]
-testInstance = [[5, 5, 5], [9, 7, 5]]
-KNN.predict(trainSet, testInstance)
