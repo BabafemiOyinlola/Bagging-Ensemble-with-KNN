5582d8ccaf732f71568403faab88735a6fb74a80
Oyinlola <babsoyinlola@yahoo.com>
2018-12-30 19:24:52 +0000

    Ensemble

--- KNN.py
+++ KNN.py
@@ -5,6 +5,8 @@ import numpy as np
 class KNN:
     def __init__ (self, k):
         self.k = k
+        self.train_feature = []
+        self.train_label = []
 
     def euclidean_distance(self, point1, point2):
         total = 0
@@ -17,14 +19,13 @@ class KNN:
         self.train_feature = train_feature
         self.train_label = train_label
 
-    def k_neighbors(self, train_set, test_set):
+    def k_neighbors(self, test_set):
         distances, neighbors = [], []
-        #iterate till len(train_set) - 1 assuming the features are in the last column
-        for i in range(len(train_set)):
-            distance = self.euclidean_distance(train_set[i], test_set)
-            train_set[i].append(self.train_label[i])
-            distances.append((train_set[i], distance))
-
+        for i in range(len(self.train_feature)):
+            distance = self.euclidean_distance(self.train_feature[i], test_set)
+            tmp = self.train_feature[i].copy()
+            tmp.append(self.train_label[i])
+            distances.append((tmp, distance))
 
         #sort the distances
         distances = sorted(distances, key=lambda tup: tup[1])
@@ -52,83 +53,111 @@ class KNN:
         return label
 
     def predict(self, test_feature):
-        test_feature = test_feature
         predicted_label = []
         for each in test_feature:
-            label = self.k_neighbors(self.train_feature, each)
-            each.append(label)
+            label = self.k_neighbors(each)
             predicted_label.append(label)
         
         return predicted_label
 
-    def confussion_matrix(self,true_label, pred_label):
-        true_classes, pred_classes = {}, {}
-        for label in true_label:
-            if label in true_classes:
-                true_classes[label] += 1
+    def confussion_matrix(self, y_test, y_pred):
+        classes = []
+        for item in y_test:
+            if item in classes:
+                continue
             else:
-                true_classes[label] == 1
+                classes.append(item)
+        
+        conf_matrix = np.array([])
+        conf_matrix = np.zeros((len(classes), len(classes)), dtype=int)
 
-        for label in pred_label:
-            if label in pred_classes:
-                pred_classes[label] += 1
-            else:
-                pred_classes[label] == 1
+        for item in range(len(y_test)):
+            actual = y_test[item]
+            pred = y_pred[item]
+
+            row = classes.index(actual)
+            col = classes.index(pred)
+
+            conf_matrix[row,col] += 1
+        
+        return conf_matrix
         
 class KNNEnsemble(KNN):
     def __init__(self, method, k):
         self.method = method
         self.k = k
+        # knn = KNN.__init__(self, k)
+        # super().__init__(self.k)
 
     def fit(self, train_feature, train_label):
         self.train_feature = train_feature
         self.train_label = train_label
 
-    def bagging(self, bags=10, split_ratio=0.6):
-            train = []
-            for i in self.train_feature:
-                temp = self.train_feature[i]
-                temp.append(self.train_label[i])
-                train.append(temp)
+    def bagging(self, X_test, label_pos, bags=10, split_ratio=0.6):
+        train = []
+
+        #combine feature and label 
+        temp = list(self.train_feature)
+        for i in range(len(temp)):
+            temp[i].append(self.train_label[i])
+            train.append(temp[i])
+        
+        predictions = []
+        #resample from the training set
+        for i in range(bags):
+            samp_num = int(split_ratio * len(train))
+            sample = []
+            #pick sample
+            for i in range(samp_num):
+                index = random.randint(0, (len(train)-1))
+                item = train[index]
+                sample.append(item)
+
+
+            #fit to classifer
+            X_train, y_train = [], []
+
+            if label_pos == -1:
+                for i in range(len(sample)):
+                    feat = sample[i]
+                    feat = feat[0:len(sample[0])-1]
+                    feat = [float(i) for i in feat]
+                    X_train.append(feat)
+                    y_train.append(sample[i][-1])
+
+            elif label_pos == 0:
+                for i in range(len(sample)):
+                    feat = sample[i]
+                    feat = feat[1:len(sample[0])]
+                    feat = [float(i) for i in feat]
+                    X_train.append(feat)
+                    y_train.append(sample[i][0])
+
+            #fit to knn classifier
+            knn = KNN(self.k)
+            knn.fit(X_train, y_train)
+            y_pred = knn.predict(X_test)
+            predictions.append(y_pred)
+        
+        voted_predictions = []
+        #vote across the predictions
+        for i in range(len(predictions)):
+            clases = {}
+            for j in predictions[i]:
+                if j in clases:
+                    clases[j] += 1
+                else:
+                    clases[j] = 1
             
-            predictions = []
-            #resample from the training set
-            for i in range(bags):
-                samp_num = int(split_ratio * len(train))
-                sample = []
-                for i in range(samp_num):
-                    index = random.randint(0, (len(train)-1))
-                    item = train[index]
-                    sample.append(item)
+            highest_vote = 0
+            label = None
+            for key, value in clases.items():
+                if value > highest_vote:
+                    highest_vote = value
+                    label = key
+            voted_predictions.append(label)
 
-                    X_train = sample[:len(sample[0])-1]   
-                    y_train = sample[:-1]
-
-                    #fit to knn classifier
-                    knn = super().__init__(self.k)
-                    knn.fit(X_train, y_train)
-                    y_pred = knn.predict(X_test)
-                    predictions.append(y_pred)
-            
-            voted_predictions = []
-            #vote across the predictions
-            for i in predictions:
-                clases = {}
-                for j in predictions[i]:
-                    if j in clases:
-                        clases[j] += 1
-                    else:
-                        clases[j] = 1
-                
-                highest_vote = 0
-                label = None
-                for key, value in clases.items():
-                    if value > highest_vote:
-                        highest_vote = value
-                        label = key
-                voted_predictions.append(label)
-
-            return voted_predictions
+        return voted_predictions
 
     
 
@@ -138,19 +167,19 @@ class KNNEnsemble(KNN):
 
 
 
-KNN = KNN(6)
-# X_train = [[7, 8, 9], [4, 4, 4],[2, 2, 2]]
-# y_train = ['a', 'b', 'c']
-# X_test = [[5, 5, 5], [9, 7, 5]]
+# KNN = KNN(6)
+# # X_train = [[7, 8, 9], [4, 4, 4],[2, 2, 2]]
+# # y_train = ['a', 'b', 'c']
+# # X_test = [[5, 5, 5], [9, 7, 5]]
 
-X_train = [[5.1,3.5,1.4,0.2], [4.9,3.0,1.4,0.2],[4.7,3.2,1.3,0.2],
-            [7.0,3.2,4.7,1.4],[6.4,3.2,4.5,1.5],[6.9,3.1,4.9,1.5],
-            [6.3,3.3,6.0,2.5],[5.8,2.7,5.1,1.9],[7.1,3.0,5.9,2.1],
-            [5.2,2.7,3.9,1.4], [4.9,3.0,1.4,0.2],[4.7,3.2,1.3,0.2]]
-y_train = ['setosa', 'setosa', 'setosa', 'versicolor', 'versicolor', 'versicolor', 'virginica', 'virginica', 'virginica', 'versicolor', 'versicolor',
-            'setosa']
-X_test = [[5.2,2.7,3.9,1.4], [4.4,3.0,1.3,0.2]]
+# X_train = [[5.1,3.5,1.4,0.2], [4.9,3.0,1.4,0.2],[4.7,3.2,1.3,0.2],
+#             [7.0,3.2,4.7,1.4],[6.4,3.2,4.5,1.5],[6.9,3.1,4.9,1.5],
+#             [6.3,3.3,6.0,2.5],[5.8,2.7,5.1,1.9],[7.1,3.0,5.9,2.1],
+#             [5.2,2.7,3.9,1.4], [4.9,3.0,1.4,0.2],[4.7,3.2,1.3,0.2]]
+# y_train = ['setosa', 'setosa', 'setosa', 'versicolor', 'versicolor', 'versicolor', 'virginica', 'virginica', 'virginica', 'versicolor', 'versicolor',
+#             'setosa']
+# X_test = [[5.2,2.7,3.9,1.4], [4.4,3.0,1.3,0.2]]
 
-KNN.fit(X_train, y_train)
-y_pred = KNN.predict(X_test)
-print("Done")
+# KNN.fit(X_train, y_train)
+# y_pred = KNN.predict(X_test)
+# print("Done")
